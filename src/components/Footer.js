8423c55f3024d0d83670299564b3efa6a584ae00
VictorMFL <vm859222@gmail.com>
2023-03-27 13:04:33 -0300

    finalizando projeto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
-import styles from '../styles/Footer.module.css'
+import React from "react";
+import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
-    return (
-      <footer className={styles.footer}>
-        <p>
-          <span>PokeNext</span> &copy; 2023
-        </p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>PokeNext</span> &copy; 2023
+      </p>
+    </footer>
+  );
+}
